074ad64823748428c1fc76a6cb01e11c2d80c9a6
ali <ali@ali.com>
2024-04-12 00:01:33 +0500

    make user api pagination

--- user/views/list.py
+++ user/views/list.py
@@ -1,12 +1,20 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from user.models import User
 from user.serializers import UserSerializer
 
 
+class UserPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
 class UserListAPIView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = UserPagination
 
     def get_serializer_context(self):
         return {"request": self.request}
